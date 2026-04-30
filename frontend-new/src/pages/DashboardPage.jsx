07deb4a1a07df7f436b5475b3ff72ad578980ad0
Ashton Benoy <ashtonbenoy1@example.com>
2026-04-30 18:54:46 +0530

    fix API import issue

--- frontend-new/src/pages/DashboardPage.jsx
+++ frontend-new/src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { API } from "../services/apiService";
+import { getEvents } from "../services/apiService";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
