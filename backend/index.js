ebf9263edb1fdbb9c1055132e31f6d2e69693732
Ashton Benoy <ashtonbenoy1@example.com>
2026-05-01 10:04:17 +0530

    backend fix

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,24 @@
 import cors from "cors";
 
 import eventRoutes from "./routes/eventRoutes.js";
+import ticketRoutes from "./routes/ticketRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Root route (for testing)
+// routes
+app.use("/api/events", eventRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+// test route
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
-
-// ✅ THIS LINE IS CRITICAL
-app.use("/api/events", eventRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-console.log("Event routes loaded");
